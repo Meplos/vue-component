872d26382d01e1ac74bfee6bdfbde47789801a17
Erard Alexandre <alexandre.erard@etu.u-bordeaux.fr>
2021-10-16 20:59:10 +0200

    Vuetify dragale chip

--- src/App.vue
+++ src/App.vue
@@ -1,59 +1,27 @@
 <template>
-  <div id="app">
-    <Progress
-      :colors="colors"
-      :max="max"
-      :score="score"
-      :threshold="threshold"
-    />
-    <br />
-    <VersusBar :colors="colors" :max="max" :score="score" />
-  </div>
+  <v-app>
+    <draggable-chip class="mt-11" :color="chipColor">
+      Hello world
+      <template v-slot:chip-actions>
+        <div>
+          <v-btn> <v-icon small>fas fa-save</v-icon> </v-btn>
+          <v-btn> <v-icon small>fas fa-trash</v-icon> </v-btn>
+        </div>
+      </template>
+    </draggable-chip>
+  </v-app>
 </template>
 
 <script>
-import Progress from "./components/Progress.vue";
-import VersusBar from "./components/VersusBar.vue";
+import DraggableChip from "./components/DraggableChip.vue";
 export default {
   name: "App",
-  components: {
-    Progress,
-    VersusBar,
-  },
+
+  components: { DraggableChip },
+
   data: () => ({
-    colors: ["#5fa85f", "#cc3732"],
-    score: 0,
-    threshold: 50,
-    max: 100,
-    timer: null,
+    chipColor: "#faee45",
+    //
   }),
-  methods: {
-    progress: function() {
-      this.timer = setInterval(() => {
-        this.score++;
-      }, 50);
-    },
-  },
-  created: function() {
-    this.progress();
-  },
-  watch: {
-    score: function(val) {
-      if (val >= this.max) {
-        clearInterval(this.timer);
-      }
-    },
-  },
 };
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/DraggableChip.vue
@@ -0,0 +1,46 @@
+<template>
+  <section>
+    <div v-if="!edit" class=".d-inline">
+      <div :style="style">
+        <v-icon class="pa-2" small>fa fa-equals</v-icon>
+        <span> <slot></slot> </span>
+      </div>
+      <slot name="chip-actions"> </slot>
+    </div>
+    <div v-else></div>
+  </section>
+</template>
+
+<script>
+export default {
+  props: {
+    dark: {
+      type: Boolean,
+      default: false,
+    },
+    edit: {
+      type: Boolean,
+      default: false,
+    },
+    color: {
+      type: String,
+      default: "#ffffff",
+    },
+  },
+  data() {
+    return {};
+  },
+  computed: {
+    style: function() {
+      let str = `background-color:${this.color};`;
+      //   str += `max-width: 300px;`;
+      str += "color:";
+      str += this.dark ? "#ffffff" : "#000000";
+
+      return str;
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
